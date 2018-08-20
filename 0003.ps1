762b68f98f113e63b199f8f9f1ff8782a76dc426
REOScotte <11577865+REOScotte@users.noreply.github.com>
2018-08-19 20:21:35 -0500

    Problem 3

--- 0003.ps1
+++ 0003.ps1
@@ -5,14 +5,19 @@ What is the largest prime factor of the number 600851475143 ?
 #>
 
 Measure-Command {
-    [int[]]$factors = $null
-    $num = 600851475143
-    for ($i = 1; $i -le $num; $i += 2) {
-        if ($num % $i -eq 0) {
-            $factors += $i
-            $num /= $i
-            $i = 1
+        [int[]]$factors = $null
+        $num = 600851475143
+        if ($num % 2 -eq 0) {
+            $factors += 2
+            $num /= 2
         }
+        for ($i = 3; $i -le [int][math]::Sqrt($num); $i += 2) {
+            if ($num % $i -eq 0) {
+                $factors += $i
+                $num /= $i
+                $i = 1
+            }
+        }
+        if ($num -gt 1) {$factors += $num}
     }
-}
 $factors | select -Last 1
