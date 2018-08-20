13d3ffed95bdad96fb097b27d0629d4f63f51362
REOScotte <11577865+REOScotte@users.noreply.github.com>
2018-08-19 20:42:23 -0500

    FIxed edge cases in problem 3

--- 0003.ps1
+++ 0003.ps1
@@ -5,19 +5,21 @@ What is the largest prime factor of the number 600851475143 ?
 #>
 
 Measure-Command {
-        [int[]]$factors = $null
-        $num = 600851475143
-        if ($num % 2 -eq 0) {
-            $factors += 2
-            $num /= 2
-        }
-        for ($i = 3; $i -le [int][math]::Sqrt($num); $i += 2) {
-            if ($num % $i -eq 0) {
-                $factors += $i
-                $num /= $i
-                $i = 1
-            }
-        }
-        if ($num -gt 1) {$factors += $num}
+    [int[]]$factors = $null
+    $num = 600851475143
+    while ($num % 2 -eq 0) {
+        $factors += 2
+        $num /= 2
     }
-$factors | select -Last 1
+    $stop = [int][math]::Sqrt($num)
+    for ($i = 3; $i -le $stop; $i += 2) {
+        if ($num % $i -eq 0) {
+            $factors += $i
+            $num /= $i
+            $stop = [int][math]::Sqrt($num)
+            $i = 3
+        }
+    }
+    $factors += $num
+}
+$factors | sort | select -Last 1
